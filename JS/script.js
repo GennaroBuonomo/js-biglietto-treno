88947909598cf2e4dacc1b6fe8a6507f547ad3e2
GennaroBuonomo <gennarobuonomo66@gmail.com>
2025-07-01 15:42:04 +0200

    [add] inizio else

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,8 @@ const prezzoalkm = 0.21;
 
 const chilometri = parseFloat(prompt("Quanrti chilometri desideri percorrere?"))
 const eta = parseInt(prompt("Quanti anni hai?"))
+
+if (isNaN(chilometri) || isNaN(eta) || chilometri <= 0 || eta <= 0)
+ 
+ } else {
+  const prezzo = chilometri * prezzoalkm
